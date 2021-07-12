af5f4d17505916e4eeda08198e23518d3004a9f9
jungwon <mac20010@naver.com>
2021-07-12 16:53:49 +0900

    프론트 엔드 반영

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import request from "request"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted(){
+   request('http://localhost:8080/api/hello', function (error, response, body){
+        window.console.log('error:',error);
+        window.console.log('statusCode:',response && response.statusCode);
+        window.console.log('body:',body);
+      });
+   }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/frontend/vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    outputDir: path.resolve(__dirname,"../"+"main/resources/static"),
+    devServer:{
+        proxy:{
+            '/api' :{
+                target: 'http://localhost:9000',
+                ws: true,
+                changeOrigin:true
+            },
+            '/member' :{
+                target: 'http://localhost:9000',
+                ws: true,
+                changeOrigin:true
+            }
+        }
+    }
+}
