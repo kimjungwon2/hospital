2bdc53a8a9dfba7c6aa575925e8a1200f2ef848b
jungwon <mac20010@naver.com>
2021-07-14 16:23:12 +0900

    frontend inital commit

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -7,20 +7,12 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import request from "request"
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  },
-  mounted(){
-   request('http://localhost:8080/api/hello', function (error, response, body){
-        window.console.log('error:',error);
-        window.console.log('statusCode:',response && response.statusCode);
-        window.console.log('body:',body);
-      });
-   }
+  }
 }
 </script>
 
