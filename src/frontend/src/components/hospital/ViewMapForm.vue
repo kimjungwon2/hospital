101bc8dd817454205ffe8649118fb0bb8d11b7df
jungwon <mac20010@naver.com>
2021-07-23 12:47:30 +0900

    지도 올바르게 출력.

--- src/frontend/src/components/hospital/ViewMapForm.vue
+++ src/frontend/src/components/hospital/ViewMapForm.vue
@@ -12,31 +12,35 @@ export default {
           }
       },
   },
-   mounted() {
-       if (window.kakao && window.kakao.maps) {
+  //detailed 값을 받아오면, watch로 변경감지해서 지도 재출력.
+  watch:{
+    detailed: function(){
+      if (window.kakao && window.kakao.maps) {
         this.initMap()
       } else {
           const script = document.createElement('script')
           script.onload = () => kakao.maps.load(this.initMap);
           script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=bcdb595a4b1c6bc005200d3b0d9271fb'
           document.head.appendChild(script)
+      }
     }
   },
   methods: {
     initMap () {
-      const container = document.querySelector('#map')
-      const options = {
-        center: new kakao.maps.LatLng(this.detailed.latitude, this.detailed.longitude),
-        level: 3
-      }
-      const map = new kakao.maps.Map(container, options)
-      const markerPosition = new kakao.maps.LatLng(this.detailed.latitude, this.detailed.longitude);
-      console.log(this.detailed.longitude);
-      console.log(this.detailed.latitude);
-      const marker = new kakao.maps.Marker({
-        position: markerPosition
+          const container = document.getElementById('map');
+          const options = {
+              center: new kakao.maps.LatLng(this.detailed.latitude, this.detailed.longitude),
+              level: 3
+          };
+
+          const map = new kakao.maps.Map(container, options)
+          const markerPosition = new kakao.maps.LatLng(this.detailed.latitude, this.detailed.longitude);
+          console.log(this.detailed.longitude);
+          console.log(this.detailed.latitude);
+          const marker = new kakao.maps.Marker({
+          position: markerPosition
       });
-      marker.setMap(map)
+           marker.setMap(map);
     },
   },
 }
