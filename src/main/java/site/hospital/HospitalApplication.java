4ebd165e16b0b9079de616aed8057b8037c727b3
jungwon <mac20010@naver.com>
2022-05-09 01:50:44 +0900

    s3 적용

--- src/main/java/site/hospital/HospitalApplication.java
+++ src/main/java/site/hospital/HospitalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class HospitalApplication{
+public class HospitalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
