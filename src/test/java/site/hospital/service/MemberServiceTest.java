ec8dd990d9c8ad55cecbf47500f87bb2f11ee273
jungwon <mac20010@naver.com>
2022-10-28 20:24:40 +0900

    회원가입 테스트2

--- src/test/java/site/hospital/service/MemberServiceTest.java
+++ src/test/java/site/hospital/service/MemberServiceTest.java
@@ -1,13 +1,10 @@
 package site.hospital.service;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 import site.hospital.domain.member.Authority;
 import site.hospital.domain.member.Authorization;
 import site.hospital.domain.member.Member;
@@ -46,7 +43,6 @@ class MemberServiceTest {
     }
 
     @Test
-    @Rollback
     public void 회원가입() {
         //given
         Member member = Member.builder().userName("이덕후")
@@ -66,5 +62,110 @@ class MemberServiceTest {
         assertEquals(member.getPhoneNumber(),findMember.get().getPhoneNumber());
         assertEquals(member.getMemberStatus(),findMember.get().getMemberStatus());
     }
+
+    @Test
+    public void 중복_회원가입(){
+        //given
+        Member member1 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName("a@naver2.com").memberStatus(MemberStatus.NORMAL)
+                .password(passwordEncoder.encode("1234")).build();
+
+
+        Member member2 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName("a@naver2.com").memberStatus(MemberStatus.NORMAL)
+                .password(passwordEncoder.encode("1234")).build();
+
+        //when
+        memberService.signUp(member1);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.signUp(member2);
+        });
+    }
+
+    @Test
+    public void 회원가입_null체크(){
+        //given
+        Member member1 = Member.builder().userName(null)
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName("a@naver2.com").memberStatus(MemberStatus.NORMAL)
+                .password(passwordEncoder.encode("1234")).build();
+
+        Member member2 = Member.builder().userName("이덕후")
+                .nickName(null).phoneNumber("01057469163")
+                .memberIdName("a@naver2.com").memberStatus(MemberStatus.NORMAL)
+                .password(passwordEncoder.encode("1234")).build();
+
+        Member member3 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber(null)
+                .memberIdName("a@naver2.com").memberStatus(MemberStatus.NORMAL)
+                .password(passwordEncoder.encode("1234")).build();
+
+        Member member4 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName(null).memberStatus(MemberStatus.NORMAL)
+                .password(passwordEncoder.encode("1234")).build();
+
+        Member member5 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName("a@naver2.com").memberStatus(null)
+                .password(passwordEncoder.encode("1234")).build();
+
+
+        //then
+        assertThrows(javax.validation.ConstraintViolationException.class, () -> {
+            memberService.signUp(member1);
+        });
+
+        assertThrows(javax.validation.ConstraintViolationException.class, () -> {
+            memberService.signUp(member2);
+        });
+
+        assertThrows(javax.validation.ConstraintViolationException.class, () -> {
+            memberService.signUp(member3);
+        });
+
+        assertThrows(javax.validation.ConstraintViolationException.class, () -> {
+            memberService.signUp(member4);
+        });
+
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Member.builder().userName("이덕후")
+                    .nickName("풉키풉키").phoneNumber("01057469163")
+                    .memberIdName("a@naver2.com").memberStatus(MemberStatus.NORMAL)
+                    .password(passwordEncoder.encode(null)).build();
+        });
+    }
+
+    //회원가입 권한 설정.(FE에서 임의로 조정해도 정상적으로 NORMAL로 되는지 확인)
+    @Test
+    public void 회원가입_권한() {
+        //given
+        Member member1 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName("a@naver2.com").memberStatus(MemberStatus.ADMIN)
+                .password(passwordEncoder.encode("1234")).build();
+
+        Member member2 = Member.builder().userName("이덕후")
+                .nickName("풉키풉키").phoneNumber("01057469163")
+                .memberIdName("a@naver3.com").memberStatus(MemberStatus.STAFF)
+                .password(passwordEncoder.encode("1234")).build();
+
+        //when
+        Long id1 = memberService.signUp(member1);
+        Long id2 = memberService.signUp(member2);
+
+        Optional<Member> findMember1 = memberRepository.findById(id1);
+        Optional<Member> findMember2 = memberRepository.findById(id2);
+
+        //then
+        assertEquals(MemberStatus.NORMAL,findMember1.get().getMemberStatus());
+        assertEquals(MemberStatus.NORMAL,findMember2.get().getMemberStatus());
+    }
+
 
 }
