53fc6660ee103b81cf268c02eb11f1e8dc263a75
jungwon <mac20010@naver.com>
2022-11-20 22:02:20 +0900

    JpaAuditingConfig 추가

---
+++ src/main/java/site/hospital/configuration/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package site.hospital.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
