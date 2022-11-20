ba9b1732a39a9a5c2f1d16c438988bd3bd39a5f8
jungwon <mac20010@naver.com>
2022-11-20 22:06:53 +0900

    JpaAuditingConfig 설정

--- src/main/java/site/hospital/HospitalApplication.java
+++ src/main/java/site/hospital/HospitalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@EnableJpaAuditing
+
 @SpringBootApplication
 public class HospitalApplication{
 	public static void main(String[] args) {
