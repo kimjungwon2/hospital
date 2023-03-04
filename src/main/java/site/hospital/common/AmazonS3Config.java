c270589255e4f0c4382739c70b9c98e001b85cbe
jungwon <mac20010@naver.com>
2023-03-05 00:07:36 +0900

    EC2 오류 메시지 제거

--- src/main/java/site/hospital/common/AmazonS3Config.java
+++ src/main/java/site/hospital/common/AmazonS3Config.java
@@ -5,10 +5,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {ContextInstanceDataAutoConfiguration.class})
 public class AmazonS3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
