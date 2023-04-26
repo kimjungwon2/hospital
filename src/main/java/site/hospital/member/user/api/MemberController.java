18023dea3e78d5a8c01e9c435f18674709f620a6
jungwon <mac20010@naver.com>
2023-04-26 22:04:35 +0900

    무중단 배포 확인(3)

--- src/main/java/site/hospital/member/user/api/MemberController.java
+++ src/main/java/site/hospital/member/user/api/MemberController.java
@@ -27,7 +27,7 @@ public class MemberController {
 
     @GetMapping("/check")
     public String checkServerStatus(){
-        return "checkV3";
+        return "checkV1";
     }
 
     @PostMapping("/login")
